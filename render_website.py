7e690caf9f7278b83c3e44806b6332d6e091429c
Nikolai124 <g4728281919@gmail.com>
2026-06-24 11:05:50 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -1,9 +1,11 @@
 import json
+import os
 
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from more_itertools import chunked
 
 
 def on_reload():
@@ -11,15 +13,21 @@ def on_reload():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    template = env.get_template('template.html')
+    template = env.get_template('template.html') 
     with open("book_info.json", "r", encoding='utf8') as file:
         books = json.load(file)
-    rendered_page = template.render(
-        books=books
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    os.makedirs("pages", exist_ok=True)
+    book_on_page = 10
+    books_pages = list(chunked(books, book_on_page))
+    for number, book_page in enumerate(books_pages):
+        rendered_page = template.render(
+            books=book_page,
+            number_pages = len(books_pages),
+            current_page = number + 1
+        )
+        with open(f'./pages/index{number + 1}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 on_reload()
 server = Server()
 server.watch('template.html', on_reload)
-server.serve(root='.')
+server.serve(root='.', default_filename='./pages/index1.html')
